1a7017b3dac3a0d806cb66bc63f30bf4e574b48e
yun-heewon <union00916@gmail.com>
2025-06-11 17:52:14 +0900

    edit_route_participant_post

--- routes/participant.js
+++ routes/participant.js
@@ -4,18 +4,44 @@ const router = express.Router({ mergeParams: true });
 
 router.post('/', async (req, res, next) => {
   try {
-    const { groupId } = req.params;
+    const groupId = Number(req.params.groupId);
     const { nickname, password } = req.body;
+
+    //groupId 유효성 검사
+    if (isNaN(groupId)) {
+      return res.status(400).json({ message: 'Invalid groupId' });
+    }
+
+    //닉네임 중복 확인
+    const nicknameChecker = await db.participant.findFirst({
+      where: {
+        groupId: groupId,
+        nickname: nickname
+      }
+    });
+
+    //닉네임 중복 시 에러처리
+    if (nicknameChecker) {
+      return res.status(409).json({ message: "Nickname already exists in this group." })
+    }
+
+    //참가자 생성
     const participant = await db.participant.create({
       data: {
-        groupId,
         nickname,
-        password
+        password,
+        group: {
+          connect: {
+            id: groupId
+          }
+        }
       },
     });
     return res.status(201).json(participant)
   } catch (error) {
-    console.log(error);
+    console.log('Error creating participant:', error);
     next(error);
   }
 });
+
+export default router;
