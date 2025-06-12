8d6670646b58c6738a9cba1ac3c0cefdfac9c105
yun-heewon <union00916@gmail.com>
2025-06-12 18:36:10 +0900

    add_group_dto

---
+++ dtos/group.dto.js
@@ -0,0 +1,17 @@
+import { object, string, size, integer, array, url, min, partial } from "superstruct";
+
+
+
+export const CreateGroup = object({
+    name: size(string(), 1, 10),
+    description: size(string(), 1, 60),
+    ownerNickname: size(string(), 1, 10),
+    ownerPassword: size(string(), 6, 15),
+    photoUrl: url(),
+    tags: size(array(size(string(), 1, 20)), 1, 10),
+    goalRep: min(integer(), 1),
+    discordWebhookUrl: url(),
+    discordInviteUrl: url()
+});
+
+export const PatchGroup = partial(CreateGroup);
