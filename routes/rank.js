239fd4ec292307534b9a1fabd1a6335e48e678ce
yun-heewon <union00916@gmail.com>
2025-06-12 16:47:59 +0900

    add_routes_rank

---
+++ routes/rank.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { db } from '../utils/db.js';
+import { RankController } from '../controller/rankController.js';
+
+const router = express.Router({ mergeParams: true });
+const rankController = new RankController(db);
+
+router.get('/:groupId/rank', rankController.getRank.bind(rankController));
+
+export default router;
