b06814af69d118b0a49328bb276870ca2decdb4a
yun-heewon <union00916@gmail.com>
2025-06-13 09:49:15 +0900

    edit_superstruct_url

--- dtos/group.dto.js
+++ dtos/group.dto.js
@@ -1,5 +1,7 @@
-import { object, string, size, integer, array, url, min, partial } from "superstruct";
+import { object, string, size, integer, array, min, partial, define } from "superstruct";
+import isurl from "is-url";
 
+const Url = define('Url', isurl)
 
 
 export const CreateGroup = object({
@@ -7,11 +9,11 @@ export const CreateGroup = object({
     description: size(string(), 1, 60),
     ownerNickname: size(string(), 1, 10),
     ownerPassword: size(string(), 6, 15),
-    photoUrl: url(),
+    photoUrl: Url,
     tags: size(array(size(string(), 1, 20)), 1, 10),
     goalRep: min(integer(), 1),
-    discordWebhookUrl: url(),
-    discordInviteUrl: url()
+    discordWebhookUrl: Url,
+    discordInviteUrl: Url
 });
 
 export const PatchGroup = partial(CreateGroup);
